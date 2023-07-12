6196b4237495be821fdbd31ae1e82d153c72e2b9
Casper Zmuda <zmudakacper03@gmail.com>
2023-07-12 21:19:26 +0200

    Created functional base of app

---
+++ index.js
@@ -0,0 +1,51 @@
+// Taki bałagan, że nawet tu nie patrz!
+
+'use strict';
+
+const progressCircle = document.querySelector('.progress');
+const percentage = document.querySelector('.progress__percentage');
+const consumedAmount = document.querySelector('.progress__consumed-amount');
+
+const dailyRequirement = document.querySelector('.daily-requirement__stat');
+const remaining = document.querySelector('.remaining__stat');
+
+const addButtons = document.querySelectorAll('button');
+
+addButtons.forEach(button =>
+	button.addEventListener('click', () =>
+		addToProgress(parseInt(button.dataset.value))
+	)
+);
+
+const DAILY = 2000;
+let amount = 0;
+let PERCENTAGE = 0;
+let remainingValue = DAILY;
+
+remaining.textContent = `${remainingValue} ml`;
+percentage.textContent = `${PERCENTAGE} %`;
+dailyRequirement.textContent = `${DAILY} ml`;
+consumedAmount.textContent = `${amount} ml`;
+
+const addToProgress = buttonValue => {
+	amount += buttonValue;
+	countPercentages(amount, DAILY);
+	remainingAmount();
+	progressCircle.style.backgroundImage = `conic-gradient(rgb(78, 123, 169) ${PERCENTAGE}%, rgb(200, 208, 215) 0%)`;
+	percentage.textContent = `${PERCENTAGE} %`;
+	dailyRequirement.textContent = `${DAILY} ml`;
+	consumedAmount.textContent = `${amount} ml`;
+};
+
+const countPercentages = (currentAmount, dailyRequirement) => {
+	PERCENTAGE = Math.floor((100 * currentAmount) / dailyRequirement);
+};
+
+const remainingAmount = () => {
+	remainingValue = DAILY - amount;
+	if (remainingValue >= 0) {
+		remaining.textContent = `${remainingValue} ml`;
+	} else {
+		remaining.textContent = `0 ml`;
+	}
+};
